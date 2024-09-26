c415e7c57e25e117277bfe3af3c01929ea02ff17
ChrisaShirauki <chrisasidirop@gmail.com>
2024-09-26 17:45:57 +0300

    Reverse Working Fine
    Need some changes for the notification messages

--- scrpt.js
+++ scrpt.js
@@ -2,7 +2,13 @@
 // Infos : zoom=18&addressdetails=1 returns the required json format for these coordinates
 
 
-/* LOCAT*/
+/* LOCATION REGGEX EXPLANING
+
++- >>> LAT & LON may be negative or positive
+([1-8]? >> LAT can take from 0-89 (+-90) while LON cat take from 0-179 (+-180) {(1[0-7]\d|[1-9]?\d)}
+d(\.\d+)? >> This is the valid formation for input
+|90(\.0+)? >> Matches exactly 90 or 90.0, allowing optional trailing zeros after the decimal point.
+*/
 //Creating function that uses Nominatim Search API : 
 
 function searchMap(){
@@ -11,41 +17,45 @@ function searchMap(){
 
     //Regular expressions for LAT & LON
     const latRegexp = /^([+-]?([1-8]?\d(\.\d+)?|90(\.0+)?))$/; 
+    const lonRegexp =/ ^([+-]?((1[0-7]\d|[1-9]?\d)(\.\d+)?|180(\.0+)?))$/;
     const apiReverse = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`;
 
     if(isNaN(lat) || isNaN(lon)){
         document.getElementById("warn").innerHTML="Invalid Input";
     }
-    
 
-   fetch(apiReverse,{   //Request from nominatim API to query the endpoit ( users location).
-       method: 'GET',  //Search operatior that is used for data gaining.
+
+
+ if(latRegexp.test(lat) && lonRegexp.test(lon)){
+    fetch(apiReverse,{   //Request from nominatim API to query the endpoit ( users location).
+       method: 'GET',  //Search operator that is used for data gaining.
        headers: {      // Expecting that the response will arrive in JSON format.
            'Accept' : 'application/json'
        }
-   })
-   .then(response=> {
-    if(!response.ok){
-        if (response.status === 404) {
-            throw new Error('404: Not Found');
-        } else if (response.status === 500) {
+    })
+    .then(response=> {
+        if(!response.ok){
+           if (response.status === 404) {
+              throw new Error('404: Not Found');
+           } else if (response.status === 500) {
             throw new Error('500: Internal Server Error');
-        } else {
-            throw new Error(`Error: ${response.status}`);
+           } else {
+              throw new Error(`Error: ${response.status}`);
+          }
         }
-    }
-    return response.json()}) //Return the json response
-   .then(data =>{                                                          //Pass json data into the modal
-      document.getElementById("result").innerHTML = JSON.stringify (data, null, 4);  //Make the Json format more readable
-   })
-   .catch(error=>{
+        return response.json()}) //Return the json response
+    .then(data =>{                                                          //Pass json data into the modal
+       document.getElementById("result").innerHTML = JSON.stringify (data, null, 4);  //Make the Json format more readable
+    })
+    .catch(error=>{
      console.error ('ERROR', error); // In case any error may happen
      document.getElementById("result").innerHTML = error.message;
-   })
+    })
 
    //open modal
    document.querySelector('.msg').style.display='flex'
-   
+}else{
+    return res.status(400).json({ error: 'Error 400- Bad Request' }); //In case user put valid
 }
 
  //close modal 
@@ -53,4 +63,4 @@ function searchMap(){
   document.querySelector('.msg').style.display='none';
  }
 
-
+}
